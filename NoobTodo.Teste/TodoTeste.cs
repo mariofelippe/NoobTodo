c788aeaa71912a7a4cb156e6e451265707d4b915
Mario felippe <mfchaves@live.com>
2022-10-22 00:28:50 -0300

    test: adiciona teste na criação de um Todo.

---
+++ NoobTodo.Teste/TodoTeste.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+using NoobTodo.Entities;
+
+namespace NoobTodo.Teste
+{
+    public class TodoTeste
+    {
+        [Fact]
+        public void CriaUmTodoValido()
+        {
+            //arrange
+            string title = "Comprar pão";
+            string description = "Comprar pão para a festa.";
+            DateTime taskDate = DateTime.Parse("10/10/2022 13:00:00");
+            bool isDone = false;
+            
+
+            //act
+            var todo = new Todo(title) {                
+                Description = description,
+                TaskDate = taskDate,
+                IsDone = isDone               
+            };
+
+            
+
+            //assert
+            Assert.Equal(title, todo.Title);
+            Assert.Equal(description, todo.Description);
+            Assert.Equal(taskDate, todo.TaskDate);
+            Assert.Equal(isDone, todo.IsDone);
+
+        }
+    }
+}
---
+++ NoobTodo/Entities/Todo.cs
@@ -0,0 +1,19 @@
+﻿
+namespace NoobTodo.Entities
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime TaskDate { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public Todo(string title)
+        {
+            Title = title;
+        }
+    }
+}
