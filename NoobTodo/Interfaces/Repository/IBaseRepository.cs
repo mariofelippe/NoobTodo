dae580efdaa476258a597bb0e43258d53f3d7abd
mariofelipp <mfchaves@live.com>
2022-10-23 14:32:05 -0300

    feat: Adiciona interface base para repositórios.

---
+++ NoobTodo/Interfaces/Repository/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace NoobTodo.Interfaces.Repository
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        public bool Add(T entity);
+        public bool Delete(int id);
+        public bool Update(int id, T entity);
+        public T GetById(int id);
+        public IEnumerable<T> GetAll();
+    }
+}
