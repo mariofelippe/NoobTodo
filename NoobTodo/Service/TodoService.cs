0272a55d9c9b96ce950e8d17383e2b4e2767ebb8
mariofelipp <mfchaves@live.com>
2022-10-23 16:08:59 -0300

    feat: Cria TodoService.

---
+++ NoobTodo/Service/TodoService.cs
@@ -0,0 +1,39 @@
+﻿using NoobTodo.Entities;
+using NoobTodo.Interfaces.Service;
+using NoobTodo.Interfaces.Repository;
+
+namespace NoobTodo.Service
+{
+    public class TodoService : IBaseService<Todo>
+    {
+        private readonly IBaseRepository<Todo> _repository;
+       public TodoService(IBaseRepository<Todo> repository)
+        {
+            _repository = repository;
+        }
+        public bool Add(Todo todo)
+        {
+            return _repository.Add(todo);
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Todo> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Todo GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
