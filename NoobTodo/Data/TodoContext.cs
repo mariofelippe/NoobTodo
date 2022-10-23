1fbb3014fcdf0d2ac3e36a8937d662c7618d90d1
mariofelipp <mfchaves@live.com>
2022-10-23 15:58:09 -0300

    feat: cria contexto de banco de dados.

---
+++ NoobTodo/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoobTodo.Entities;
+
+namespace NoobTodo.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options)
+        {
+
+        }
+
+        DbSet<Todo> Todos { get; set; }
+    }
+}
