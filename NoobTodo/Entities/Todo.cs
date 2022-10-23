2b981c605bc8beb64fda773023e7b13b2151807c
Mario felippe <mfchaves@live.com>
2022-10-23 13:15:27 -0300

    feat: Gera exceção para title null ou vazio.

--- NoobTodo/Entities/Todo.cs
+++ NoobTodo/Entities/Todo.cs
@@ -4,7 +4,16 @@ namespace NoobTodo.Entities
     public class Todo
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        private string _title;
+        public string Title 
+        { 
+            get { return _title; }
+            set { 
+                if(value == string.Empty || value == null)                
+                    throw new ArgumentException("Titulo não informado!");                
+                _title = value;
+            }
+        }
         public string Description { get; set; }
         public DateTime TaskDate { get; set; }
         public bool IsDone { get; set; }
