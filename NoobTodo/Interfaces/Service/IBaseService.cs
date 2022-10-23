c17fcf9f910f70cb52168ff8d23500407ff0e19f
mariofelipp <mfchaves@live.com>
2022-10-23 16:14:15 -0300

    feat: Adiciona interface base para serviço.

--- NoobTodo/Interfaces/Service/IBaseService.cs
+++ NoobTodo/Interfaces/Service/IBaseService.cs
@@ -1,7 +1,13 @@
 ﻿
 namespace NoobTodo.Interfaces.Service
 {
-    internal interface IBaseService
+    public interface IBaseService<T> where T : class
     {
+        public bool Add(T entity);
+        public bool Delete(int id);
+        public bool Update(int id, T entity);
+        public T GetById(int id);
+        public IEnumerable<T> GetAll();
+       
     }
 }
