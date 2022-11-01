829e4941fcf5b29cf62408cb71e6180c2cbf0580
mariofelipp <mfchaves@live.com>
2022-10-31 22:47:11 -0300

    feat: Adiciona o retorno dos todos na API

---
+++ NoobTodo.Api/Controllers/TodoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoobTodo.Entities;
+using NoobTodo.Data;
+using NoobTodo.Interfaces.Repository;
+using NoobTodo.Interfaces.Service;
+using NoobTodo.Service;
+
+namespace NoobTodo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+
+        private readonly TodoRepository _repository;
+        private readonly TodoService _service;
+        
+        public TodoController(TodoContext context)
+        {
+            _repository = new TodoRepository(context);
+            _service = new TodoService(_repository);
+        }
+        [HttpGet]
+        public IEnumerable<Todo> GetTodos()
+        {
+            return _service.GetAll();
+        }
+    }
+}
