bbfd34356335a505f3d00c60207542b584b89d1a
mariofelipp <mfchaves@live.com>
2022-10-31 22:48:13 -0300

    feat: Adiciona DbContex MySql.

--- NoobTodo.Api/Program.cs
+++ NoobTodo.Api/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NoobTodo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(options =>
+options.UseMySql(builder.Configuration.GetConnectionString("TodoConnection"), new MySqlServerVersion(new Version(8, 0)), b => b.MigrationsAssembly("NoobTodo.Api")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
