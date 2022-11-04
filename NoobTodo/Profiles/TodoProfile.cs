1a5e87e74f07d9eb516537daec5537b827d6304b
mariofelipp <mfchaves@live.com>
2022-11-04 20:28:24 -0300

    feat: Adiciona Profile para Todo.

---
+++ NoobTodo/Profiles/TodoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NoobTodo.DTOs;
+using NoobTodo.Entities;
+
+namespace NoobTodo.Profiles
+{
+    public class TodoProfile : Profile
+    {
+       public TodoProfile()
+        {
+            CreateMap<CreateTodoDto, Todo>();
+            CreateMap<Todo, CreateTodoDto>();
+        }
+    }
+}
