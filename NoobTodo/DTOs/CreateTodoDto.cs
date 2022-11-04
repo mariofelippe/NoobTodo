af916c67144bf65bd213c747c83e3f4232e44064
mariofelipp <mfchaves@live.com>
2022-11-04 20:00:06 -0300

    feat: Adiciona CreateTodoDto.

---
+++ NoobTodo/DTOs/CreateTodoDto.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace NoobTodo.DTOs
+{
+    public class CreateTodoDto
+    {
+        [Required(ErrorMessage = "O titulo deve ser informado!")]
+        public string Title { get ; set; }
+        public string? Description { get; set; }
+        public DateTime? TaskDate { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
