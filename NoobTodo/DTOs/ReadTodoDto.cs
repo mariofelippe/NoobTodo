49d105f0228b7a3e7c343192a9f57a7fc8e9836d
mariofelipp <mfchaves@live.com>
2022-11-04 22:15:32 -0300

    feat: Adiciona DTO para leitura de Todo.

---
+++ NoobTodo/DTOs/ReadTodoDto.cs
@@ -0,0 +1,35 @@
+﻿
+namespace NoobTodo.DTOs
+{
+    public class ReadTodoDto
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? TaskDate { get; set; }
+        public bool IsDone { get; set; }
+        
+        public bool IsLate {
+
+            get
+            {
+                if (TaskDate.HasValue)
+                {
+                    bool late = TaskDate.Value < DateTime.Now && !IsDone ? true : false;
+                    return late;
+                }
+                return false;
+            }
+           
+        }
+        public TimeSpan? TimeDelay
+        {
+            get
+            {
+                var timeDelay = IsLate ? DateTime.Now - TaskDate : DateTime.Now - DateTime.Now;
+                return timeDelay;
+            }
+        }
+        public DateTime CreatedAt { get; set; } 
+        public DateTime UpdatedAt { get; set; } 
+    }
+}
