49f3dd2a0747dee1d9c74453ad20aaa75d8bf0d5
mariofelipp <mfchaves@live.com>
2022-11-04 21:00:11 -0300

    feat: Adiciona DTO para atualização do Todo.

---
+++ NoobTodo/DTOs/UpdateTodoDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoobTodo.DTOs
+{
+    public class UpdateTodoDto
+    {
+        [Required(ErrorMessage = "O titulo deve ser informado!")]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? TaskDate { get; set; }
+        public bool IsDone { get; set; }        
+        public DateTime UpdatedAt { get; } = DateTime.Now;
+    }
+}
