5d94e21635784a27bd01d5f8b1173af1fc9750f4
mariofelipp <mfchaves@live.com>
2022-11-08 23:11:34 -0200

    test: Adiciona teste para TodoList.

---
+++ NoobTodo.Teste/TodoListTeste.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using NoobTodo.Entities;
+
+namespace NoobTodo.Teste
+{
+    public class TodoListTeste
+    {
+        [Fact]
+        public void DeveriaCriarUmaListaDeTaregas()
+        {
+            // arrange
+            string title = "Não iniciado";
+            string description = "Tarefas que não ainda não foram iniciadas";
+            //act
+            TodoList listaTarefas = new TodoList(title){  Description = description };
+            // arrange
+            Assert.Equal(title, listaTarefas.Title);
+            Assert.Equal(description, listaTarefas.Description);
+
+        }
+    }
+}
