b7a3860369185c50d3500e0dc55e92fdf0f21cf4
mariofelipp <mfchaves@live.com>
2022-11-08 23:12:47 -0200

    feat: Cria TodoList.

---
+++ NoobTodo/Entities/TodoList.cs
@@ -0,0 +1,18 @@
+﻿
+namespace NoobTodo.Entities
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public List<Todo> Todos { get; set; } = new List<Todo>();
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public TodoList(string title)
+        {
+            Title = title;
+        }
+    }
+}
