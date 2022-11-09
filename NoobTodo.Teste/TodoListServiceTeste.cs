322b244880096b15de9f4d433f58fe772e25634b
mariofelipp <mfchaves@live.com>
2022-11-09 00:02:22 -0200

    test: Adiciona teste para TodoListSerice.

---
+++ NoobTodo.Teste/TodoListServiceTeste.cs
@@ -0,0 +1,42 @@
+﻿using NoobTodo.Service;
+using NoobTodo.Data;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using NoobTodo.Entities;
+using System.Collections.Generic;
+
+namespace NoobTodo.Teste
+{
+    public class TodoListServiceTeste
+    {
+        private readonly TodoListService _service;
+        private readonly TodoListRepository _repository;
+        private readonly TodoContext _context;
+        private static DbContextOptions<TodoContext> dbContextOptions = new DbContextOptionsBuilder<TodoContext>()
+            .UseInMemoryDatabase("noob").Options;
+
+        public TodoListServiceTeste()
+        {
+            _context = new TodoContext(dbContextOptions);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            _repository = new TodoListRepository(_context);
+            _service = new TodoListService(_repository);
+        }
+
+        [Fact]
+        public void DeveriaAdicionarUmaLista()
+        {
+            //arrange
+            TodoList lista = new TodoList("Lista 0");
+            Todo todo = new Todo("Comprar pão");
+            lista.AddTodo(todo);
+
+            //act
+            var adicionou = _service.Add(lista);
+            //assert
+            Assert.True(adicionou);
+
+        }
+    }
+}
