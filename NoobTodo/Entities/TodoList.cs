8c2e42474e4d21f5faef99b7a2a0eee68ce637ab
mariofelipp <mfchaves@live.com>
2022-11-12 16:53:36 -0200

    feat: Adicionar ajuste para geração de migrations.

--- NoobTodo/Entities/TodoList.cs
+++ NoobTodo/Entities/TodoList.cs
@@ -6,7 +6,7 @@ namespace NoobTodo.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public string? Description { get; set; }
-        public List<Todo> Todos { get; set; } = new List<Todo>();
+        public virtual List<Todo> Todos { get; set; } = new List<Todo>();
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
