a2894f7bfd12b2641255ea0b1cda649ecebbab59
mariofelipp <mfchaves@live.com>
2022-11-12 16:56:28 -0200

    feat: Adiciona id no DTO.

--- NoobTodo/DTOs/ReadTodoDto.cs
+++ NoobTodo/DTOs/ReadTodoDto.cs
@@ -1,8 +1,11 @@
 ﻿
+using NoobTodo.Entities;
+
 namespace NoobTodo.DTOs
 {
     public class ReadTodoDto
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime? TaskDate { get; set; }
