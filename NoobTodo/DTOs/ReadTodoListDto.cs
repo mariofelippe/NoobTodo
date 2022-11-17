ca9bef5e05383b0199927c9f66729f8943dae731
mariofelipp <mfchaves@live.com>
2022-11-17 21:56:05 -0200

    feat: Adicionar Dto para lista de tarefas.

---
+++ NoobTodo/DTOs/ReadTodoListDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace NoobTodo.DTOs
+{
+    public class ReadTodoListDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public List<ReadTodoDto> Todos { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
