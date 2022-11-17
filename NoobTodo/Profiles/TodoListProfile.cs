aad7750e01ca00e2f993121dbf0cb3d929e318b7
mariofelipp <mfchaves@live.com>
2022-11-17 21:56:44 -0200

    feat: Adiciona profile para TodoList.

---
+++ NoobTodo/Profiles/TodoListProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NoobTodo.DTOs;
+using NoobTodo.Entities;
+
+namespace NoobTodo.Profiles
+{
+    public class TodoListProfile : Profile
+    {
+        public TodoListProfile()
+        {
+            CreateMap<TodoList,ReadTodoListDto>();
+        }
+    }
+}
