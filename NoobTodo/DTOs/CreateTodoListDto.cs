bfc7e69b291b124c25e7fdd79c51bcc47cd94e3c
mariofelipp <mfchaves@live.com>
2022-11-24 20:51:35 -0200

    feat: Adiciona DTO para criação de lista de tarefas.

---
+++ NoobTodo/DTOs/CreateTodoListDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoobTodo.DTOs
+{
+    public class CreateTodoListDto
+    {
+        [Required(ErrorMessage="O titulo deve ser informado.")]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
